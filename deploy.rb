2e63fe6a3f9cf2677a7700f38604f923c2e022f2
Nicholas Chapman <nick@indigorenderer.com>
2023-10-18 02:24:22 +1300

    Updated deploy.rb to use GLARE_CORE_TRUNK_DIR.

--- deploy.rb
+++ deploy.rb
@@ -1,12 +1,5 @@
 require 'pathname'
-
-def exec_command(cmd)
-	res = Kernel.system(cmd)
-	if !res
-		STDERR.puts "Command failed: " + cmd
-		exit(1)
-	end
-end
+require './script_utils.rb'
 
 
 
@@ -24,42 +17,24 @@ end
 #	copy_file(f, "website/" + f)
 #end
 
-indigo_trunk_dir = ENV["INDIGO_TRUNK_DIR"]
-puts "indigo_trunk_dir: #{indigo_trunk_dir}"
-raise "indigo_trunk_dir not found." if !indigo_trunk_dir
+glare_core_trunk_dir = ENV["GLARE_CORE_TRUNK_DIR"]
+puts "glare_core_trunk_dir: #{glare_core_trunk_dir}"
+raise "glare_core_trunk_dir not found." if !glare_core_trunk_dir
 	
 
 exec_command("sevenz a src.zip src")
 exec_command("sevenz a src.zip systemd")
 exec_command("sevenz a src.zip CMakeLists.txt")
-exec_command("sevenz a src.zip N:/website_core/trunk")
-exec_command("sevenz a src.zip #{indigo_trunk_dir}/double-conversion")
-exec_command("sevenz a src.zip #{indigo_trunk_dir}/utils")
-exec_command("sevenz a src.zip #{indigo_trunk_dir}/networking")
-exec_command("sevenz a src.zip #{indigo_trunk_dir}/maths/mathstypes.h")
+exec_command("sevenz a src.zip #{glare_core_trunk_dir}/webserver")
+exec_command("sevenz a src.zip #{glare_core_trunk_dir}/double-conversion")
+exec_command("sevenz a src.zip #{glare_core_trunk_dir}/utils")
+exec_command("sevenz a src.zip #{glare_core_trunk_dir}/networking")
+exec_command("sevenz a src.zip #{glare_core_trunk_dir}/maths/mathstypes.h")
 #exec_command("sevenz a src.zip #{indigo_trunk_dir}/maths/SSE.cpp")
 #exec_command("sevenz a src.zip #{indigo_trunk_dir}/maths/SSE.h")
 #exec_command("sevenz a src.zip #{indigo_trunk_dir}/indigo/TestUtils.h")
 #exec_command("sevenz a src.zip #{indigo_trunk_dir}/indigo/globals.h")
 copy_file("src.zip", "website/src.zip")
 
-networking_files = Dir.glob("#{indigo_trunk_dir}/networking/*")
+networking_files = Dir.glob("#{glare_core_trunk_dir}/networking/*")
 
-networking_files.each do |path|
-	filename = Pathname.new(path).basename
-	#copy_file(path, "website/indigo/networking/" + filename.to_s)
-end
-
-util_files = Dir.glob("#{indigo_trunk_dir}/utils/*")
-
-util_files.each do |path|
-	filename = Pathname.new(path).basename
-	#copy_file(path, "website/indigo/utils/" + filename.to_s)
-end
-
-double_conversion_files = Dir.glob("#{indigo_trunk_dir}/double-conversion/*")
-
-double_conversion_files.each do |path|
-	filename = Pathname.new(path).basename
-	#copy_file(path, "website/indigo/double-conversion/" + filename.to_s)
-end
